dc9111ba0dff91f7c7fba96c19646bea252ab991
Dpac99 <diogo.pacheco@tecnico.ulisboa.pt>
2023-02-20 22:43:31 +0000

    smth

--- function/entry.sh
+++ function/entry.sh
@@ -3,4 +3,4 @@ if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   exec /usr/bin/aws-lambda-rie "$@"
 else
   exec "$@"
-fi
+fi 
--- shim/index.js
+++ shim/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 1234
+const bodyParser = require('body-parser');
 const crypto = require('crypto');
 var Docker = require('dockerode')
+
+
+const app = express()
+app.use(bodyParser.json({ extended: true }));
+const port = 1234
+
 var docker = new Docker();
 let image
 
@@ -15,27 +20,35 @@ async function loadImages() {
 
 
 let workers = []
+let pool = []
 
 app.get('/', (req, res) => {
   res.send('Hello World2!')
 })
 
 app.post('/register', (req, res) => {
-  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
   var uuid = crypto.randomUUID()
   console.log(uuid)
-  workers.push(ip)
+  console.log(req.body)
+  workers.push({
+    "UUID": uuid,
+    "Memory": null,
+    "CPU": null,
+    "nTasks": 1,
+  })
   console.log(workers)
   res.send(JSON.stringify(uuid))
 })
 
 app.post('/poll', (req, res) => {
-  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-  console.log(req)
-  if (workers.includes(ip)) {
-    console.log('received poll request')
-    res.send('ok')
-  }
+  let stats = req.body
+  let index = workers.findIndex((w) => w.UUID === stats.UUID)
+  workers[index].Memory = stats.Memory
+  workers[index].CPU = stats.CPU
+})
+
+app.post('/invoke', (req, res) => {
+
 })
 
 app.listen(port, () => {
