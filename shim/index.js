7741a349faccc9b9b50864be7f68496c774f7d3d
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2023-04-11 02:34:46 +0100

    fixed payload limit 3

--- shim/index.js
+++ shim/index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(express.json)
 const port = config.get('server.port')
 const url = config.get('server.host')
 
